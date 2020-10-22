73fbdc5203c25eb6da014403f1847bcdbcba2859
shigeyuki3 <zatosige@gmail.com>
2020-10-22 10:30:34 +0900

    mv_sp

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
                     <img src="~@/static/img/type.png" alt="update">
                 </div>
                 <!--SP-->
-            <div class="w-full bg-cover bg-center sm:hiden" style="height:18rem; background-image: url(https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80);">
+            <div class="w-full bg-cover bg-center sm:hidden" style="height:18rem; background-image: url(https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80);">
             </div>
                 <h3 class="text-xl mb-8 text-gray-200">
                     ＵＰＤＯＯＲＳは時代に合ったＵＰＤＡＴＥを続けてまいります
